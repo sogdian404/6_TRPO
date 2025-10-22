01f6a0738593e7b116c9d883fb2f68c5e310def4
XamiD <130274873+sogdian404@users.noreply.github.com>
2025-10-22 09:59:00 +0500

    Add files via upload

--- 6_TRPO/obj/Debug/net8.0-windows/MainWindow.g.cs
+++ 6_TRPO/obj/Debug/net8.0-windows/MainWindow.g.cs
@@ -53,7 +53,7 @@ namespace _6_TRPO {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/6_TRPO;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/6_TRPO;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
